7bd73a49be5e4636f09a9508878eb67ac94acf0d
masa2401 <239096414+masa2401@users.noreply.github.com>
2026-08-29 13:40:41 +0900

    CI/CD関連改善

--- vue-frontend/playwright.config.ts
+++ vue-frontend/playwright.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: process.env.CI ? 'npm run build && npm run preview -- --port 5173' : 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
     env: {
